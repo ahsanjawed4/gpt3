82afdb469d02d5af0f0b627efeabbbd23f54891e
Ahsan_4 <ahsanjawed2001@gmail.com>
2022-04-01 23:58:25 +0500

    edit name

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -43,7 +43,7 @@ const Footer = () => (
     </div>
 
     <div className="gpt3__footer-copyright">
-      <p>@2021 GPT-3. All rights reserved.</p>
+      <p>@2021 GPT-3. All rights reserved.Ahsan__4</p>
     </div>
   </div>
 );
